258e3a12081e2b6e7c698587bc69ead035ff19ae
George Grasham <geoed2000@gmail.com>
2017-10-12 23:51:21 +0100

    Added the NoceClass

--- OOP Homework 2/OOP Homework 2/NodeClass.cs
+++ OOP Homework 2/OOP Homework 2/NodeClass.cs
@@ -6,6 +6,16 @@ namespace OOP_Homework_2
 {
     class NodeClass
     {
+        private string data;
+        public string Data
+        {
+            set { data = value; }
+            get { return data; }
+        }
 
+        public NodeClass(string inData)
+        {
+            data = inData;
+        }
     }
 }
